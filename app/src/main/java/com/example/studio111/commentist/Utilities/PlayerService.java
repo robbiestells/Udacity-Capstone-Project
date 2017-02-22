b3ba31650cd99a8da5dd9f38abe2f8b2ee271684
robbiestells <robbiestells@hotmail.com>
2017-02-21 22:20:42 -0500

    feat: Add audiomanager and audio focus
    Added audiomanager to playerservice to
    handle loss in audio focus

--- app/src/main/java/com/example/studio111/commentist/Utilities/PlayerService.java
+++ app/src/main/java/com/example/studio111/commentist/Utilities/PlayerService.java
@@ -2,6 +2,7 @@ package com.example.studio111.commentist.Utilities;
 
 import android.app.Activity;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -19,9 +20,12 @@ import com.example.studio111.commentist.R;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.concurrent.TimeUnit;
 
 import com.example.studio111.commentist.Objects.FeedItem;
 
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
 import static com.example.studio111.commentist.R.id.seekBar;
 
 /**
@@ -64,119 +68,157 @@ public class PlayerService extends Service {
 
     //public void LoadUrl(String url, final SeekBar seekBar, Activity activity) {
     public void LoadUrl(FeedItem feedItem, Activity activity) {
-        if (mediaPlayer != null) {
-            mediaPlayer.stop();
-            mediaPlayer.release();
-
-        }
-        mediaPlayer = new MediaPlayer();
-
-
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        try {
-            mediaPlayer.setDataSource(feedItem.getAudioUrl());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            mediaPlayer.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        mediaPlayer.start();
-        mediaPlayer.setOnCompletionListener(mCompletionListener);
-
-        mainActivity = activity;
-
-        mSeekBar = (SeekBar) activity.findViewById(seekBar);
-        mSeekBar.setMax(100);
-
-        totalTimeTV = (TextView) activity.findViewById(R.id.totalTime);
-        totalTimeTV.setText(feedItem.getLength());
-
-        playerEpisodeDesc = (TextView) activity.findViewById(R.id.playerEpisodeDesc);
-        playerEpisodeDesc.setText(feedItem.getDescription());
-
-        currentTimeText = (TextView) activity.findViewById(R.id.currentTime);
-        mainActivity.runOnUiThread(new Runnable() {
+        AudioManager am = (AudioManager) activity.getSystemService(Context.AUDIO_SERVICE);
+        AudioManager.OnAudioFocusChangeListener afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
             @Override
-            public void run() {
-                if (mediaPlayer != null) {
-                    long mCurrentPosition = mediaPlayer.getCurrentPosition() / 1000;
-                    long maxPosition = mediaPlayer.getDuration() / 1000;
-
-                    BigDecimal max = new BigDecimal(maxPosition);
-                    BigDecimal mult = new BigDecimal(100);
-                    BigDecimal current = new BigDecimal(mCurrentPosition);
-                    BigDecimal progress = current.divide(max, 3, RoundingMode.CEILING);
-                    BigDecimal percent = progress.multiply(mult);
-
-                    mSeekBar.setProgress(percent.intValue());
-
-                    currentTimeText.setText(getTimeString(mCurrentPosition * 1000));
-
+            public void onAudioFocusChange(int focusChange) {
+                if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                    // Permanent loss of audio focus
+                    // Pause playback immediately
+                    Pause();
                 }
-                mHandler.postDelayed(this, 1000);
-            }
-        });
-
-        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                if (mediaPlayer != null) {
-                    //get percentage seeked
-                    int i = seekBar.getProgress();
-                    double maxPosition = mediaPlayer.getDuration() * .01;
-                    //figure out new percentage and progress
-                    double newPercentage = maxPosition * i;
-                    int newPosition = (int) newPercentage;
-                    //seek to new progress
-                    mediaPlayer.seekTo(newPosition);
+                else if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT) {
+                    // Pause playback
+                    if (mediaPlayer.isPlaying()) {
+                        Pause();
+                    }
+                } else if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                    // Lower the volume, keep playing
+                    if (mediaPlayer.isPlaying()) {
+                        Pause();
+                    }
+                } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                    // Your app has been granted audio focus again
+                    // Raise volume to normal, restart playback if necessary
+                    if (!mediaPlayer.isPlaying()) {
+                        Pause();
+                    }
                 }
             }
-        });
+        };
 
-        playpauseButton = (FloatingActionButton) activity.findViewById(R.id.playpause);
-        playpauseButton.setImageResource(R.drawable.pause);
-        playpauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Pause();
+        int result = am.requestAudioFocus(afChangeListener,
+                // Use the music stream.
+                AudioManager.STREAM_MUSIC,
+                // Request permanent focus.
+                AudioManager.AUDIOFOCUS_GAIN);
+
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+
+            if (mediaPlayer != null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+
             }
-        });
+            mediaPlayer = new MediaPlayer();
 
-        forwardButton = (ImageButton) activity.findViewById(R.id.seekForwardButton);
-        forwardButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SkipForward();
+
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                mediaPlayer.setDataSource(feedItem.getAudioUrl());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
 
-        backButton = (ImageButton) activity.findViewById(R.id.seekBackButton);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SkipBack();
+            try {
+                mediaPlayer.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(mCompletionListener);
 
-        //update widget
-        Intent dataUpdatedIntent = new Intent(ACTION_DATA_UPDATED);
-        dataUpdatedIntent.putExtra("showTitle", feedItem.getTitle());
-        dataUpdatedIntent.putExtra("show", feedItem.getShow());
-        dataUpdatedIntent.putExtra("player", PlayerService.class);
-        activity.sendBroadcast(dataUpdatedIntent);
+            mainActivity = activity;
 
+            mSeekBar = (SeekBar) activity.findViewById(seekBar);
+            mSeekBar.setMax(100);
+
+            totalTimeTV = (TextView) activity.findViewById(R.id.totalTime);
+            totalTimeTV.setText(feedItem.getLength());
+
+            playerEpisodeDesc = (TextView) activity.findViewById(R.id.playerEpisodeDesc);
+            playerEpisodeDesc.setText(feedItem.getDescription());
+
+            currentTimeText = (TextView) activity.findViewById(R.id.currentTime);
+            mainActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (mediaPlayer != null) {
+                        long mCurrentPosition = mediaPlayer.getCurrentPosition() / 1000;
+                        long maxPosition = mediaPlayer.getDuration() / 1000;
+
+                        BigDecimal max = new BigDecimal(maxPosition);
+                        BigDecimal mult = new BigDecimal(100);
+                        BigDecimal current = new BigDecimal(mCurrentPosition);
+                        BigDecimal progress = current.divide(max, 3, RoundingMode.CEILING);
+                        BigDecimal percent = progress.multiply(mult);
+
+                        mSeekBar.setProgress(percent.intValue());
+
+                        currentTimeText.setText(getTimeString(mCurrentPosition * 1000));
+
+                    }
+                    mHandler.postDelayed(this, 1000);
+                }
+            });
+
+            mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    if (mediaPlayer != null) {
+                        //get percentage seeked
+                        int i = seekBar.getProgress();
+                        double maxPosition = mediaPlayer.getDuration() * .01;
+                        //figure out new percentage and progress
+                        double newPercentage = maxPosition * i;
+                        int newPosition = (int) newPercentage;
+                        //seek to new progress
+                        mediaPlayer.seekTo(newPosition);
+                    }
+                }
+            });
+
+            playpauseButton = (FloatingActionButton) activity.findViewById(R.id.playpause);
+            playpauseButton.setImageResource(R.drawable.pause);
+            playpauseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Pause();
+                }
+            });
+
+            forwardButton = (ImageButton) activity.findViewById(R.id.seekForwardButton);
+            forwardButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SkipForward();
+                }
+            });
+
+            backButton = (ImageButton) activity.findViewById(R.id.seekBackButton);
+            backButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    SkipBack();
+                }
+            });
+
+            //update widget
+            Intent dataUpdatedIntent = new Intent(ACTION_DATA_UPDATED);
+            dataUpdatedIntent.putExtra("showTitle", feedItem.getTitle());
+            dataUpdatedIntent.putExtra("show", feedItem.getShow());
+            dataUpdatedIntent.putExtra("player", PlayerService.class);
+            activity.sendBroadcast(dataUpdatedIntent);
+
+        }
     }
 
     public void Pause() {
