485709527751916e7db1bc820cd92744a929c369
rob <robbiestells@hotmail.com>
2017-02-23 09:58:44 -0500

    style: Changed colors, added splash screen

---
+++ app/src/main/java/layout/SplashScreen.java
@@ -0,0 +1,44 @@
+package layout;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.prime.perspective.commentist.MainActivity;
+import com.prime.perspective.commentist.R;
+
+/**
+ * Created by rob on 2/23/17.
+ */
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
